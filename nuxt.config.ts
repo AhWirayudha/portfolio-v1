a57985544815369a8c711c3911bccec9535fcd6c
ahwirayuda <wirayudha@impstudio.co.id>
2025-03-24 15:53:58 +0700

    update favicon and meta title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,4 +5,18 @@ export default defineNuxtConfig({
   css: ['./assets/app.css'],
   pages: true,
   compatibilityDate: '2025-03-12',
+  app: {
+    head: {
+      title: 'Ah Wirayudha - Software Engineer',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: 'Ah Wirayudha - Software Engineer' },
+      ],
+      htmlAttrs: {
+        lang: 'en',
+      },
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+  },
 });
