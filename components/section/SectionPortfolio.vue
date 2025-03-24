55879aab0dbbef5ee1dc29b6886d722c4172909b
ahwirayuda <wirayudha@impstudio.co.id>
2025-03-24 15:08:26 +0700

    section portfolio

--- components/section/SectionPortfolio.vue
+++ components/section/SectionPortfolio.vue
@@ -11,16 +11,76 @@
       </p>
     </div>
     <div class="relative grid grid-cols-1 gap-4 p-4 lg:grid-cols-3">
-      <div class="card">
-        <h2 class="card-title">Card title</h2>
-        <p class="card-body">This is a card component.</p>
+      <div
+        class="h-fit max-w-sm rounded-lg border border-gray-200 bg-white shadow-sm dark:border-gray-700 dark:bg-gray-800"
+      >
+        <a href="#">
+          <img
+            class="rounded-t-lg"
+            src="/assets/images/hcm_dashboard.png"
+            alt=""
+          />
+        </a>
+        <div class="p-5">
+          <a href="#">
+            <h5
+              class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+            >
+              HCM Recruitment Dashboard
+            </h5>
+          </a>
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            HCM Recruitment Dashboard is an application for viewing recruitment
+            data in chart form to make it easier to read the data. This
+            application is built using HTML, CSS, JavaScript and ChartJs.
+          </p>
+        </div>
       </div>
-      <div class="card-dark">
-        <h3 class="card-title">Writes upside-down</h3>
-        <p class="card-body">
-          The Zero Gravity Pen can be used to write in any orientation,
-          including upside-down. It even works in outer space.
-        </p>
+      <div
+        class="h-fit max-w-sm rounded-lg border border-gray-200 bg-white shadow-sm dark:border-gray-700 dark:bg-gray-800"
+      >
+        <a href="#">
+          <img
+            class="rounded-t-lg"
+            src="/assets/images/appsheetrengel.png"
+            alt=""
+          />
+        </a>
+        <div class="p-5">
+          <a href="#">
+            <h5
+              class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+            >
+              AppSheet Rengel Tuban
+            </h5>
+          </a>
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            AppSheet Rengel Tuban is a monitoring application for the work of
+            the public works department in the Rengel area of Tuban Regency.
+            This application is built using Laravel.
+          </p>
+        </div>
+      </div>
+      <div
+        class="h-fit max-w-sm rounded-lg border border-gray-200 bg-white shadow-sm dark:border-gray-700 dark:bg-gray-800"
+      >
+        <a href="#">
+          <img class="rounded-t-lg" src="/assets/images/porgan.jpg" alt="" />
+        </a>
+        <div class="p-5">
+          <a href="#">
+            <h5
+              class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+            >
+              Poros Ganjaran
+            </h5>
+          </a>
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Poros Ganjaran is an Android-based mobile application built with
+            React Native. This application is a monitoring activity of Poros
+            Ganjaran volunteers during the 2024 presidential election.
+          </p>
+        </div>
       </div>
     </div>
   </section>
